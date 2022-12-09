941a52324cf8456ce597503775af027fb04e75b3
Emile Girard <emilegirard42@gmail.com>
2022-12-09 17:19:27 -0500

    startform

--- frontend/src/Components/Item.js
+++ frontend/src/Components/Item.js
@@ -6,7 +6,7 @@ function Item({Item})
     return(
         <div>
             <img src={Item.ImageURL} alt={Item.Title} width="150"/><br/>
-            <Button variant={Item.Favorite? "warning":"secondary"} size="sm">Favorite</Button>
+            <Button className="rounded-circle rounded-sm" variant={Item.Favorite? "warning":"secondary"} size="sm"></Button>
             <Button variant="danger" size="sm">Delete</Button>
             <h2>{Item.Title}</h2>
             <h3>{Item.Type}</h3>
--- frontend/src/Components/ItemList.js
+++ frontend/src/Components/ItemList.js
@@ -3,27 +3,44 @@ import { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import Item from "./Item";
 import { useForm } from 'react-hook-form';
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 function ItemList(){
 
-    const [users, setUsers] = useState([]);
-    const [items, setItems] = useState([]);
+    {//const [users, setUsers] = useState([]);
+    //const [items, setItems] = useState([]);
+    }
 
-    const { register, handleSubmit, watch, formState: { errors }} = useForm(); 
-    const onSubmit = data => {
-        setUsers(data);
-        addUser(users);
-        console.log(data);
+    const [rating, setRating] = useState(0)
+
+    const users = [];
+    
+    const items = [{Id: 1, Title: "titanic", Type: 1, ImageURL: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png", Description: "desc", Rating: 3, Favorite: true},
+    {Id: 1, Title: "titanic", Type: 1, ImageURL: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png", Description: "desc", Rating: 3, Favorite: true},
+    {Id: 1, Title: "titanic", Type: 1, ImageURL: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png", Description: "desc", Rating: 3, Favorite: true}];
+
+    const { register, handleSubmit, formState: { errors }} = useForm();
+    const { register: register2, handleSubmit: handleSubmit2, formState: { errors: errors2 }} = useForm();  
+    const submitNewUser = data => {
+        {//setUsers(data);
+        //ddUser(users);
+    }
+    console.log("test1");
+}
+    const submitNewItem = data => {
+        console.log("test2");
     }
 
     useEffect(()=>{
-        
-    },[])
+        console.log(rating);
+    },[rating])
 
     const loadUsers = ()=>{
         axios.get("http://localhost:8080/api/users")
         .then(response => {
-            setUsers(response.data);
+            {//setUsers(response.data);
+            }
         });
     }
 
@@ -36,36 +53,70 @@ function ItemList(){
             console.log(error);
         })
     }
-
-    
-    
     return(
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input {...register("firstName", { required: true })}/>
-                <input {...register("lastName", { required: true })} />
-                <input {...register("userName", { required: true })} />
-                {errors.exampleRequired && <span>This field is required</span>}
-                <input type="submit"/>
-            </form>
-            
+        <div className=" bgcolor">
+            <div className="items">
 
             <Row>
             {items.map((i) => (
                 i.Favorite &&
-                <Col xs={3}>
+                <Col lg="auto" md="auto" sm="auto">
                     <Item key={i.Id} Item={{Title: i.Title, Type: i.Type, ImageURL: i.ImageURL, Description: i.Description, Rating: i.Rating, Favorite: i.Favorite}}/>
                 </Col>
             ))}
+            <Col>
+            
+            </Col>
             </Row>
             <Row>
             {items.map((i) => (
                 !i.Favorite &&
-                <Col xs={3}>
-                    <Item key={i.Id} Item={{Title: i.Title, Type: i.Type, ImageURL: i.ImageURL, Description: i.Description, Rating: i.Rating, Favorite: i.Favorite}}/>
+                <Col>
+                    <Item className="item" key={i.Id} Item={{Title: i.Title, Type: i.Type, ImageURL: i.ImageURL, Description: i.Description, Rating: i.Rating, Favorite: i.Favorite}}/>
                 </Col>
             ))}
             </Row>
+
+            </div>
+            <div className="form">
+
+            <Row>
+            <div className="addUser forms">
+                <h3>Add User</h3>
+                <form className="float-right" onSubmit={handleSubmit(submitNewUser)}>
+                    <label>First Name</label><br/>
+                    <input {...register("firstName", { required: true })}/>
+                    {errors.firstName && <span>This field is required</span>} <br/>
+                    <label>Last Name</label><br/>
+                    <input {...register("lastName", { required: true })} />
+                    {errors.lastName && <span>This field is required</span>} <br/>
+                    <label>User Name</label><br/>
+                    <input {...register("userName", { required: true })} />
+                    {errors.userName && <span>This field is required</span>} <br/>
+                    <input className="button rounded-pill" type="submit" value="Add User"/>
+                </form>
+            </div>
+            </Row>
+            <Row>
+            <div className="addUser forms">
+                <h3>Add Item</h3>
+                <form onSubmit={handleSubmit2(submitNewItem)}>
+                    <label>Title</label><br/>
+                    <input {...register2("title", { required: true })}/>
+                    {errors2.title && <span>This field is required</span>} <br/>
+                    <label>Description</label><br/>
+                    <input {...register2("description", { required: true })} />
+                    {errors2.description && <span>This field is required</span>} <br/>
+                    <label>Image URL</label><br/>
+                    <input {...register2("imageURL", { required: true })} />
+                    {errors2.imageURL && <span>This field is required</span>} <br/>
+                    <Rating style={{ maxWidth: 150 }} value={rating} onChange={setRating} />
+                    <input className="button rounded-pill" type="submit" value="Add Item"/>
+                </form>
+            </div>
+            </Row>
+
+            </div>
         </div>
     )
 }
